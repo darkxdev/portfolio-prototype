7ddf461f6fc5ac43bf2a8e56a0539d76e3918a16
JakariaJishan <jakariajishan987@gmail.com>
2022-12-20 04:34:16 +0600

    Change position into flex

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ let mobileMenu = document.querySelector('#mobile_menu')
 let closeIcon = document.querySelector('#close_icon')
 
 menuBtn.addEventListener('click', () => {
-  mobileMenu.style.display = 'block'
+  mobileMenu.style.display = 'flex'
 })
 
 closeIcon.addEventListener('click', () => {
