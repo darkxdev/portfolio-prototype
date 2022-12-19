8ed3e662fc15a6656c88c14e20f4d96e44e14a88
DarkxDev <bryanfernandohurtado@gmail.com>
2022-12-19 17:45:24 -0500

    Install Linters and mobile menu positioning

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ let mobileMenu = document.querySelector('#mobile_menu')
 let closeIcon = document.querySelector('#close_icon')
 
 menuBtn.addEventListener('click', () => {
-  mobileMenu.style.display = 'flex'
+  mobileMenu.style.display = 'block'
 })
 
 closeIcon.addEventListener('click', () => {
