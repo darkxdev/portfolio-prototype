9a4252e34dbf6704566096c02ed1f698e7941296
DarkxDev <bryanfernandohurtado@gmail.com>
2022-12-19 20:09:01 -0500

    Added EventListener to list items

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let menuBtn = document.querySelector('#menu')
 let mobileMenu = document.querySelector('#mobile_menu')
 let closeIcon = document.querySelector('#close_icon')
+let listItems = document.querySelectorAll('.list_item');
 
 menuBtn.addEventListener('click', () => {
   mobileMenu.style.display = 'block'
@@ -9,3 +10,10 @@ menuBtn.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
   mobileMenu.style.display = 'none'
 })
+
+for (let i = 0; i < listItems.length; i++) {
+  let element = listItems[i];
+  element.addEventListener('click', ()=>{
+      mobileMenu.style.display='none'   
+  })
+}
