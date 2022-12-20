b3e5c10c9f96da7ad389f596ecba6d1450b38160
DarkxDev <bryanfernandohurtado@gmail.com>
2022-12-20 10:56:44 -0500

    fix linters

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
-let menuBtn = document.querySelector('#menu')
-let mobileMenu = document.querySelector('#mobile_menu')
-let closeIcon = document.querySelector('#close_icon')
-let listItems = document.querySelectorAll('.list_item');
+const menuBtn = document.querySelector('#menu');
+const mobileMenu = document.querySelector('#mobile_menu');
+const closeIcon = document.querySelector('#close_icon');
+const listItems = document.querySelectorAll('.list_item');
 
 menuBtn.addEventListener('click', () => {
-  mobileMenu.style.display = 'block'
-})
+  mobileMenu.style.display = 'block';
+});
 
 closeIcon.addEventListener('click', () => {
-  mobileMenu.style.display = 'none'
-})
+  mobileMenu.style.display = 'none';
+});
 
-for (let i = 0; i < listItems.length; i++) {
-  let element = listItems[i];
-  element.addEventListener('click', ()=>{
-      mobileMenu.style.display='none'   
-  })
+for (let i = 0; i < listItems.length; i += 1) {
+  const element = listItems[i];
+  element.addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
+  });
 }
